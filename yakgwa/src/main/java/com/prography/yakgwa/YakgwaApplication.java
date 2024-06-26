b99744d065b08c5a93823043f1d476c690710f5a
jaepyo99 <jaepyo99@naver.com>
2024-06-26 16:25:29 +0900

    init

---
+++ yakgwa/src/main/java/com/prography/yakgwa/YakgwaApplication.java
@@ -0,0 +1,13 @@
+package com.prography.yakgwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakgwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakgwaApplication.class, args);
+	}
+
+}
---
+++ yakgwa/src/test/java/com/prography/yakgwa/YakgwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prography.yakgwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakgwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
