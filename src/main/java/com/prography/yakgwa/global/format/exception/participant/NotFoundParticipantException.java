2d90eeeb8e71d91884ce851444408d77d5090864
jaepyo99 <jaepyo99@naver.com>
2024-07-13 20:28:07 +0900

    feat(#40): 조회되지 않는 사용자의 모임 참여내역에 대한 예외처리

--- src/main/java/com/prography/yakgwa/global/format/exception/participant/NotFoundParticipantException.java
+++ src/main/java/com/prography/yakgwa/global/format/exception/participant/NotFoundParticipantException.java
@@ -4,15 +4,16 @@ import com.prography.yakgwa.global.format.exception.ApplicationRunException;
 import com.prography.yakgwa.global.format.exception.ErrorEnumCode;
 
 import static com.prography.yakgwa.global.format.exception.participant.errorCode.ParticipantErrorCode.ALREADY_PARTICIPANT_IN_MEET_EXCEPTION;
+import static com.prography.yakgwa.global.format.exception.participant.errorCode.ParticipantErrorCode.NOT_FOUND_PARTICIPANT_EXCEPTION;
 
-public class AlreadyParticipantInMeetException extends ApplicationRunException {
-    private static final ErrorEnumCode CODE = ALREADY_PARTICIPANT_IN_MEET_EXCEPTION;
+public class NotFoundParticipantException extends ApplicationRunException {
+    private static final ErrorEnumCode CODE = NOT_FOUND_PARTICIPANT_EXCEPTION;
 
-    private AlreadyParticipantInMeetException(ErrorEnumCode CODE) {
+    private NotFoundParticipantException(ErrorEnumCode CODE) {
         super(CODE);
     }
 
-    public AlreadyParticipantInMeetException() {
+    public NotFoundParticipantException() {
         this(CODE);
     }
 }
