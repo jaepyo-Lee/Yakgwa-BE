38a2e8bf65af1464edbdc58b3a3dba0ed0d30918
jaepyo99 <jaepyo99@naver.com>
2024-07-16 01:01:50 +0900

    feat(#34): 매거진 스웨거수정

--- src/main/java/com/prography/yakgwa/domain/magazine/controller/MagazineAdminApi.java
+++ src/main/java/com/prography/yakgwa/domain/magazine/controller/MagazineAdminApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Magazine_Admin", description = "[관리자] 매거진과 관련된 토큰관련 API입니다.")
+@Tag(name = "Magazine_Admin", description = "[관리자] 매거진과 관련된 API입니다.")
 public interface MagazineAdminApi {
     @Operation(summary = "매거진생성 API", description = "매거진 생성 API 관리자 권한의 사용자만 허용됌")
     SuccessResponse<CreateMagazineResponse> create(@RequestPart CreateMagazineRequest createMagazineRequest,
--- src/main/java/com/prography/yakgwa/domain/magazine/controller/MagazineApi.java
+++ src/main/java/com/prography/yakgwa/domain/magazine/controller/MagazineApi.java
@@ -3,7 +3,12 @@ package com.prography.yakgwa.domain.magazine.controller;
 import com.prography.yakgwa.domain.magazine.controller.res.CreateMagazineResponse;
 import com.prography.yakgwa.domain.magazine.controller.res.PagingMagazineResponse;
 import com.prography.yakgwa.global.format.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Magazine_User", description = "[사용자]매거진과 관련된 API입니다.")
 public interface MagazineApi {
+
+    @Operation(summary = "매거진 조회 API", description = "페이지네이션적용, 모든 사용자 접근가능")
     SuccessResponse<PagingMagazineResponse> findAllByPaging(int pageNum);
 }
